cbaed334b043f6ab4cef7e62cf6f8675f506be42
gki-webik <gki-webik@yandex.ru>
2025-01-01 02:33:59 +0600

    upd

--- pages/checkmate.vue
+++ pages/checkmate.vue
@@ -14,6 +14,7 @@
         <div class="square is-3"></div>
         <div class="square is-4"></div>
       </div>
+      <div class="right"></div>
     </div>
     <div class="block2">
       <div class="top"><div class="square"></div></div>
@@ -47,6 +48,102 @@
         </p>
       </div>
       <img src="@/public/assets/media/2.png" alt="img" />
+    </div>
+    <div class="block4">
+      <h2>Хронология работы платформы</h2>
+      <div class="left"></div>
+      <div class="circle is-1"></div>
+      <div class="circle is-2"></div>
+      <div class="line">
+        <div class="horizonatal">
+          <div class="circle is-3"></div>
+          <div class="circle is-4"></div>
+          <div class="circle is-5"></div>
+        </div>
+        <div class="vertical is-1">
+          <div class="circle is-6"></div>
+        </div>
+        <div class="vertical is-2">
+          <div class="circle is-7"></div>
+        </div>
+        <div class="vertical is-3">
+          <div class="circle is-8"></div>
+        </div>
+      </div>
+      <div class="text is-1">
+        <h3>Регистрация</h3>
+        <p>
+          Владельцы компании создают учетную запись и заполняют профиль
+          стандартами и чеклистами, которые могут быть автоматически
+          сгенерированы и редактируемы.
+        </p>
+      </div>
+
+      <div class="text is-2">
+        <h3>Задания</h3>
+        <p>
+          Компания публикует задания для тайных покупателей, которые посещают
+          заведение, оценивают по чек-листу и оставляют отзыв, не раскрывая свою
+          роль.
+        </p>
+      </div>
+
+      <div class="text is-3">
+        <h3>Отчеты и рекомендации</h3>
+        <p>
+          Платформа автоматически рассчитывает оценки и предлагает рекомендации
+          по улучшению сервиса, на основе отчетов тайных покупателей.
+        </p>
+      </div>
+    </div>
+    <div class="block5">
+      <div class="top">
+        <div class="square is-1"></div>
+        <div class="square is-2"></div>
+        <h2>Преимущества платформы</h2>
+      </div>
+      <div class="center">
+        <div class="items">
+          <div class="item">
+            <div class="title">
+              <img src="@/public/assets/media/6.png" alt="" /> Объективные
+              данные
+            </div>
+            <p>
+              Платформа обеспечивает доступ к объективным данным от реальных
+              клиентов без дополнительных затрат.
+            </p>
+          </div>
+          <div class="item">
+            <div class="title">
+              <img src="@/public/assets/media/9.png" alt="" /> Контроль сервиса
+            </div>
+            <p>
+              Бизнес получает инструменты для контроля и улучшения сервиса,
+              включая рекомендации и аналитику.
+            </p>
+          </div>
+          <div class="item">
+            <div class="title">
+              <img src="@/public/assets/media/8.png" alt="" /> Рост прибыли
+            </div>
+            <p>
+              Компании могут увеличить выручку, улучшая обслуживание и внедряя
+              дополнительные продажи.
+            </p>
+          </div>
+          <div class="item">
+            <div class="title">
+              <img src="@/public/assets/media/7.png" alt="" /> Экономия времени
+            </div>
+            <p>
+              Платформа помогает быстро выявлять и улучшать слабые места,
+              экономя время на обучении персонала.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="circle is-1"></div>
     </div>
   </div>
 </template>
