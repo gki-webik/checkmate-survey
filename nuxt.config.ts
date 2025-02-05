ef72501fd4a680cbd5c81ea894697af2519e65d3
gki-webik <gki-webik@yandex.ru>
2025-02-05 23:40:01 +0600

    pd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   plugins: ["~/plugins/vue-the-mask.js"],
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: false }
-})
+  compatibilityDate: "2024-11-01",
+  ssr: false,
+  devtools: { enabled: false },
+});
